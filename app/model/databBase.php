1d9a8daaa656c4ac46aaea3ffab4cec6d742a42a
Kyle Bentley <bentleykyle@protonmail.com>
2026-02-03 09:57:25 -0500

    integrate databse into model

--- app/model/databBase.php
+++ app/model/databBase.php
@@ -3,8 +3,8 @@
 class dataBase
 {
     private $host = "localhost";
-    private $db_name = ""; //database name
-    private $username = ""; //database username
+    private $db_name = "complaint_system"; //database name
+    private $username = "root"; //database username
     private $password = ""; //database password
     public $conn; //connection variable
 
@@ -12,10 +12,44 @@ class dataBase
     {
         $this->conn = null;
         try {
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+            // Connect without database
+            $this->conn = new mysqli(
+                $this->host, 
+                $this->username,
+                $this->password, 
+                );
+
             if ($this->conn->connect_error) {
                 throw new Exception("Connection error: " . $this->conn->connect_error);
             }
+
+            // Create Database if it does not exist
+            $this->conn->query(
+                "CREATE DATABASE IF NOT EXISTS {$this->db_name}
+                CHARACTER SET utf8
+                COLLATE utf8"
+                );
+
+            // select DB
+            $this->conn->select_db($this->db_name);
+
+            // load DB
+            $schemaPath = __DIR__ . '/../../database/database.sql';
+            if (!file_exists($schemaPath)) {
+                throw new Exception("database.sql not found");
+            }
+
+            $sql = file_get_contents($schemaPath);
+
+            if (!$this->conn->multi_query($sql)) {
+                throw new Exception("Schema execution failed: " . $this->conn->error);
+            }
+
+            // Clear multi_query results
+            while ($this->conn->more_results()) {
+                $this->conn->next_result();
+            }
+
             $this->conn->set_charset("utf8");
         } catch (Exception $exception) {
             echo "Connection error: " . $exception->getMessage();
